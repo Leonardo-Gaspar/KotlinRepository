7d966037cea702355c34d0da1cb906a813d3c12a
Leonardo Gaspar Saheb <leosaheb2003@gmail.com>
2024-08-26 20:26:25 -0300

    Update

--- FOR/NumbersListChallenge.kt
+++ FOR/NumbersListChallenge.kt
@@ -4,17 +4,14 @@
 fun main() {
 
     val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) //Criando a lista
-    
-    
-    var sumNumbers = 0 // armazenar a soma
 
-    
+    var sumNumbers = 0 // armazenar a soma
+  
     for (number in numbers) { //Fazendo a soma
         if (number % 2 == 0) { //Pegando apenas os pares
             sumNumbers += number
         }
     }
 
-    
     println("A soma dos números pares na lista é: $sumNumbers")
 }
